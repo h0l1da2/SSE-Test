1895826536654a10b3022f05c1234e213a4d829d
h0l1da2 <whaleee@naver.com>
2024-02-23 17:11:33 +0900

    feat: add stream-flux event
    - add stream-flux event
    - The streamFlux is sent LocalDateTime.now() on client
    - Duration of 1 seconds
    
    holiday

--- src/main/java/com/example/sse/controller/SseTestController.java
+++ src/main/java/com/example/sse/controller/SseTestController.java
@@ -1,8 +1,23 @@
 package com.example.sse.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-@Controller
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/sse")
 public class SseTestController {
+
+    @GetMapping(value = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        // 서버에 초 단위로 현재 시간을 보내는 이벤트
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux Event : " + LocalTime.now());
+    }
 
 }
